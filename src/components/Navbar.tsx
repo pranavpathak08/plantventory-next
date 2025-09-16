0e3f7baf1b30c7175b05040b74f33bc0ea4d5410
pranavpathak08 <ppathak2002@gmail.com>
2025-09-16 13:13:42 +0530

    homepage ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,42 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
-import { Sprout } from 'lucide-react';
+import { Sprout, HomeIcon } from 'lucide-react';
+import ModeToggle from "./ModeToggle";
 
 export default function Navbar() {
     return (
         <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
-            {/* Logo */}
             <div className="max-w-7xl mx-auto px-4">
-                <div className="flex items-center">
-                    <Link
-                        href="/"
-                        className="text-xl font-bold text-primary font-mono tracking-wider"
-                    >Planetventory</Link>
+                <div className="flex items-center h-16 justify-between">
+                    {/* Logo */}
+                    <div className="flex items-center">
+                        <Link
+                            href="/"
+                            className="text-xl font-bold text-primary font-mono tracking-wider"
+                        > 🌱 Planetventory</Link>
+                    </div>
+                
+
+                    {/* Navbar components */}
+                    <div className="hidden md:flex items-center space-x-4">
+                        <Button variant="ghost" className="flex items-center gap-2" asChild>
+                            <Link href="/plants">
+                                <Sprout className="w-4 h-4" />
+                                <span className="hidden lg:inline">Plant</span>
+                            </Link>
+                        </Button>
+
+                        <Button variant="ghost" className="flex items-center gap-2" asChild>
+                            <Link href="/">
+                                <HomeIcon className="w-4 h-4" />
+                                <span className="hidden lg:inline">Home</span>
+                            </Link>
+                        </Button>
+
+                        <ModeToggle />
+                    </div>
                 </div>
             </div>
-
-            {/* Navbar components */}
-            <div className="hidden md:flex items-center space-x-4">
-                <Button variant="ghost" className="flex items-center gap-2" asChild>
-                    <Link href="/plants">
-                        <Sprout className="w-4 h-4" />
-                        <span className="hidden lg:inline">Plant</span>
-                    </Link>
-                </Button>
-            </div>
-
         </nav>
     )   
 }
